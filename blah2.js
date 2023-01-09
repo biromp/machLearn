a8b76033d469c13bf8c24d06bd7ac0b5adcce284
biromp <biromp6@protonmail.com>
2023-01-10 01:57:01 +0200

    Create blah2.js

---
+++ blah2.js
@@ -0,0 +1,10 @@
+export var data = {
+  series: [
+    { name: 'Brand', type: 'dimension', values: ['ALFA_ROMEO', 'ALFA_ROMEO', 'ALFA_ROMEO']
+ },
+    { name: 'County', type: 'dimension', values: ['AB', 'AR', 'AG']
+ },
+    { name: 'Data', type: 'measure', values:  [3, 14, 4]},
+    { name: 'index', type: 'measure', values: (new Array(3)).fill(1) }
+  ]
+};
